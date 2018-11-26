ffe7d6d4e1d73fd378e9ae7e50861fb4a59811be
Carlos Contreras <kradvenko@outlook.com>
2018-11-26 14:16:34 -0700

    Más ventanas

---
+++ EfectivoInmediato/cDepartamento.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EfectivoInmediato
+{
+    public class cDepartamento
+    {
+        public String IdDepartamento { get; set; }
+        public String Departamento { get; set; }
+    }
+}
