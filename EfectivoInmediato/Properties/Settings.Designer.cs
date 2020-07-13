7e6f359218804405b227b370369c8b9680f2d5f1
kradvenko <kradvenko@gmail.com>
2020-07-12 20:12:01 -0600

    qewqe

--- EfectivoInmediato/Properties/Settings.Designer.cs
+++ EfectivoInmediato/Properties/Settings.Designer.cs
@@ -12,7 +12,7 @@ namespace EfectivoInmediato.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "15.9.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "16.6.0.0")]
     internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
         
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
@@ -26,8 +26,8 @@ namespace EfectivoInmediato.Properties {
         [global::System.Configuration.ApplicationScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
         [global::System.Configuration.SpecialSettingAttribute(global::System.Configuration.SpecialSetting.ConnectionString)]
-        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=USER\\SQL2005;Initial Catalog=EfectivoInmediato;Integrated Security=Tr" +
-            "ue;User ID=USER\\USUARIO")]
+        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=DESKTOP-5F75Q2N\\SQLEXPRESS;Initial Catalog=EfectivoInmediato;Integrat" +
+            "ed Security=True;User ID=USER\\USUARIO")]
         public string EfectivoInmediatoConnectionString {
             get {
                 return ((string)(this["EfectivoInmediatoConnectionString"]));
